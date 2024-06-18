fd303b8709174e891035854cc109a4957eccecc1
jarthc20 <jarthc20@htl-kaindorf.at>
2024-06-18 21:11:17 +0200

    tests

--- cool_backend/cypress/e2e/users_api.cy.js
+++ cool_backend/cypress/e2e/users_api.cy.js
@@ -1,16 +1,70 @@
 describe('songs',  () => {
-    it ("OK: GET ALL Users,", async () => {
+    it ("OK: GET ALL songs,", async () => {
         //Exp
         const expectedStatus = 200;
-        const expectedName = "Gabriel";
+        const expectedSize = 10;
 
         //Action
-        const response = await cy.request("GET","http://localhost:3000/users/1");
+        const response = await cy.request("GET","http://localhost:30000/songs/all");
         const actualStatus = response.status;
-        const actualUser = response.body;
+        const actualSongs = response.body;
 
         //Assertion
         expect(actualStatus).to.equal(expectedStatus)
-        expect(actualUser.name).to.equal(expectedName)
+        expect(actualSongs.length).to.equal(expectedSize)
     })
+
+    it ("OK: GET ALL songs byPlaylist,", async () => {
+        //Exp
+        const expectedStatus = 200;
+        const expectedSize = 0;
+
+        //Action
+        const responsePlaylist = await cy.request("GET","http://localhost:30000/playlists/all");
+        const actualStatusPlaylists = responsePlaylist.status;
+        const playlists = responsePlaylist.body;
+
+        expect(actualStatusPlaylists).to.equal(expectedStatus)
+
+        const response = await cy.request("GET","http://localhost:30000/songs/all?byPlaylist=" + playlists[0]._id);
+
+        cy.wait(500)
+
+        const actualStatus = response.status;
+        const actualSongs = response.body;
+
+        //Assertion
+        expect(actualStatus).to.equal(expectedStatus)
+        expect(actualSongs.length).to.equal(expectedSize)
+    })
+
+    it ("OK: GET ALL songs byPlaylist,", async () => {
+        //Exp
+        const expectedStatus = 200;
+        const expectedSize = 0;
+
+        //Action
+        const response = await cy.request("GET","http://localhost:30000/songs/all");
+        const actualStatus = response.status;
+        const actualSongs = response.body;
+        cy.wait(500)
+
+
+        const responsePlaylist = await cy.request("GET","http://localhost:30000/playlists/all");
+        const actualStatusPlaylists = responsePlaylist.status;
+        const playlists = responsePlaylist.body;
+
+        cy.wait(500)
+
+        //Assertion
+
+        const response2 = await cy.request("POST","http://localhost:30000/songs/addToPlaylist?playlist="+playlists[0]._id,JSON.stringify(actualSongs[0]))
+        cy.wait(500)
+        expect(actualStatusPlaylists).to.equal(expectedStatus)
+        expect(actualStatus).to.equal(expectedStatus)
+        expect(response2.status).to.equal(expectedStatus)
+
+
+    })
+
 });
